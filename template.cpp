1c1360976a0513f5212b6bb9840ba700d84bfdae
gokulcodes <gokul282@outlook.com>
2021-05-28 18:07:30 +0530

    template_updates

--- template.cpp
+++ template.cpp
@@ -3,6 +3,7 @@
 // ABOUT: COMPETITIVE PROGRAMMING TEMPLATE
 
 #include <bits/stdc++.h>
+#include <functional>
 
 using namespace std;
 
@@ -10,37 +11,39 @@ typedef long long LL;
 #define br '\n'
 
 #define mem(a, b) memset(a, b, sizeof(a))
-#define REP(i, j, range, inc) for(int i = j; i < range; i+=inc)
-#define RREP(i, j, range, inc) for(int i = j; i >= range; i-=inc)
+#define rep(i, j, range, inc) for(int i = j; i < range; i+=inc)
+#define rrep(i, j, range, inc) for(int i = j; i >= range; i-=inc)
 #define all(a) a.begin(), a.end()
+#define read(v) for(auto &el : v) cin >> el;
 #define rall(a) a.end(), a.begin()
-#define ITR(it, i, j) for(auto it = i; it != j; it++)
-#define print(a) cout << a << '\n'
+#define itr(it, i, j) for(auto it = i; it != j; it++)
+#define print(a) cout << (a) << br
+#define show(a, b) for(auto it = a; it != b; it++) cout << *it << " ";
 #define printd(a,b) cout << a << " " << b << '\n'
 #define printt(a,b,c) cout << a << " " << b << " " << c << '\n'
-#define debl(arr) REP(i, 0, (int)arr.size(), 1) cout << arr[i] << br;
+#define debl(arr) rep(i, 0, (int)arr.size(), 1) cout << arr[i] << " ";
 #define deb(name, a) cout << name << " " << a << br;
-#define MP make_pair
-#define PB push_back
-#define F first
-#define S second
-#define MOD 1000000007
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+LL MOD = 1e9+7;
 
-typedef pair<int, int> PI;
-typedef pair<LL, LL> PLL;
-typedef vector<int> VI;
-typedef vector<VI> VII;
-typedef vector<LL> VL;
-typedef vector<VL> VLL;
-typedef vector<string> VS;
-typedef vector<PI> VPI;
-typedef vector<PLL> VPL;
-typedef map<int, int> MII;
-typedef map<LL, LL> MLL;
-typedef set<int> SI;
-typedef set<LL> SL;
-typedef multiset<int> MSETI;
-typedef multiset<LL> MSETL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<LL> vl;
+typedef vector<vl> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+typedef map<int, int> mii;
+typedef map<LL, LL> mll;
+typedef set<int> si;
+typedef set<LL> sl;
+typedef multiset<int> msi;
+typedef multiset<LL> msl;
 
 typedef long int int32;
 typedef unsigned long int uint32;
@@ -48,19 +51,19 @@ typedef long long int int64;
 typedef unsigned long long int uint64;
 
 
-VI isPrime(1e6, 1);
+vi isPrime(20, 1);
 void seive(int range){
 
 	isPrime[0] = isPrime[1] = 0;
 
-	for(int i = 2; i*i <= range; i++)
+		for(int i = 2; i*i <= range; i++)
 		if(isPrime[i])
 			for(int j = i*i; j <= range; j+=i)
-				isPrime[j] = 0;
+				isPrime[j] = i;
 }
 
-uint64 mod_expo(uint64 n, uint64 exp, LL p = MOD){
-	uint64 res = 1;
+LL mod_expo(LL n, LL exp, LL p = MOD){
+	LL res = 1;
 
 	while(exp>0){
 		if(exp&1) {res = ((res%p) * (n%p)) % p; exp--;};
@@ -71,27 +74,24 @@ uint64 mod_expo(uint64 n, uint64 exp, LL p = MOD){
 	return res;
 }
 
-function<int(int, int)> gcd = [](int a, int b) {
-    return b == 0 ? a : gcd(b, a % b);
-};
-
-//==========================================//
-
-function<int(string, int, int)> palindrome = [](string s, int a, int b) {
-    if(a == b) return 1;
-    if(s[a] != s[b]) return 0;
-    if(a >= b) return 1;
-
-    palindrome(s, a+1, b-1);
+int checkPrime(int n){
+	
+	if(n == 1 or n == 0) return -1;
+	
+	for(int i = 2; i*i <= n; i++){
+		if(n % i == 0){
+			return -1;
+		}
+	}
 	return 1;
-};
+}	
+
+// ================================================ //
 
 void solve(){
-	
 	// TODO
 	// solve cp problems
 }
-
 
 int main(){
 	ios_base::sync_with_stdio(false);
@@ -119,7 +119,7 @@ int main(){
 
 	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 
-	cout << "Duration: " << elapsed_seconds.count() << "s\n";
+	// cout << "Duration: " << elapsed_seconds.count() << "s\n";
 
 	return 0;
 }
